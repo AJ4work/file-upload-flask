182b854e4fd595db39c690b5539a0f5bfdf33c5f
AJ4work <130941497+AJ4work@users.noreply.github.com>
2025-04-16 11:12:28 +0800

    Add activation of Python virtual environment in start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,6 @@
 # Start your Flask app here (adjust as needed)
 echo "Starting Flask application..."
 cd /home/ec2-user/file-upload-flask
+# Activate Python virtual environment
+source venv/bin/activate
 flask --app main run --host 0.0.0.0 &
